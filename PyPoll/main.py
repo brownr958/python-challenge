c081b20346514f77474dad45ec7efc03a6c322ee
Ryan Brown <brownr958@gmail.com>
2020-12-08 16:33:32 -0500

    more updates to main

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,6 +18,7 @@ with open(election_csv, 'r') as csvfile:
     header = next(csvreader)
     # Setting each of the variables to be used in the loop below
     count = 0
+    candidate_list=[]
     khan_count=0
     correy_count=0
     li_count=0
@@ -37,7 +38,17 @@ with open(election_csv, 'r') as csvfile:
             li_count+=1
         elif row[2]=="O'Tooley":
             otooley_count+=1
-        #Calculating the percentage of votes for Khan
+        #Calculating the percentage of votes for each candidate
+        khan_percent=round((khan_count/count)*100,4)
+        correy_percent=round((correy_count/count)*100,4)
+        otooley_percent=round((otooley_count/count)*100,4)
+        li_percent=round((li_count/count)*100,4)
+        #Formatting results to be to 3 decimal places
+        khan="{:.3f}".format(khan_percent)
+        correy="{:.3f}".format(correy_percent)
+        otooley="{:.3f}".format(otooley_percent)
+        li="{:.3f}".format(li_percent)
+        
 
 
     #Print out the results
@@ -45,9 +56,12 @@ with open(election_csv, 'r') as csvfile:
     print("---------------------------------")
     print(f"Total Votes: {count}")
     print("---------------------------------")
-    print(f"Khan: ({khan_count})")
-    print(f"Correy: ({correy_count})")
-    print(f"Li: ({li_count})")
-    print(f"O'Tooley: ({otooley_count})")
+    print(f"Khan: {khan}% ({khan_count})")
+    print(f"Correy: {correy}% ({correy_count})")
+    print(f"Li: {li}% ({li_count})")
+    print(f"O'Tooley: {otooley}% ({otooley_count})")
+    print("---------------------------------")
+    print(f'Winner: Khan')
+    print("---------------------------------")
 
         
