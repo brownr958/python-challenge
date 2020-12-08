c39d846cfa38d32be66288af146887b627d3c167
Ryan Brown <brownr958@gmail.com>
2020-12-08 15:42:51 -0500

    Updating the main file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,12 +7,28 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-budget_csv = os.path.join('Resources','budget_data.csv')
+election_csv = os.path.join('Resources','election_data.csv')
 
 # Read in the CSV file
-with open(budget_csv, 'r') as csvfile:
+with open(election_csv, 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
     print((csvreader))
     header = next(csvreader)
+    # Setting each of the variables to be used in the loop below
+    count = 0
+
+
+    # Loop through each of the csv file
+    for row in csvreader:
+        #Counting the number of votes
+        count +=1
+
+    #Print out the results
+    print("Election Results")
+    print("---------------------------------")
+    print(f"Total Votes: {count}")
+    print("---------------------------------")
+
+        
