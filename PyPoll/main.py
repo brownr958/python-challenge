8ae7a1657ad88ea09933d39228ac1f18c733c0f8
Ryan Brown <brownr958@gmail.com>
2020-12-09 12:57:14 -0500

    Updating main file and the analysis output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,83 +9,68 @@ import csv
 # Path to collect data from the Resources folder
 election_csv = os.path.join('Resources','election_data.csv')
 
+    # Setting each of the variables to be used in the loop below
+count = 0
+candidate_list= []
+candidates = {}
+
 # Read in the CSV file
 with open(election_csv, 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
-    print((csvreader))
+    # Setting the first row to be the column names
     header = next(csvreader)
-    # Setting each of the variables to be used in the loop below
-    count = 0
-    candidate_list=set()
-    khan_count=0
-    correy_count=0
-    li_count=0
-    otooley_count=0
 
 
     # Loop through each of the csv file
     for row in csvreader:
         #Counting the number of votes
         count +=1
-        #Creating list of candidates
-        candidate_list.add(row[2])
-        #Counting the number of votes for each candidate
-        if row[2]=="Khan":
-            khan_count+=1
-        elif row[2]=="Correy":
-            correy_count+=1
-        elif row[2]=="Li":
-            li_count+=1
-        elif row[2]=="O'Tooley":
-            otooley_count+=1
-        #Calculating the percentage of votes for each candidate
-        khan_percent=round((khan_count/count)*100,4)
-        correy_percent=round((correy_count/count)*100,4)
-        otooley_percent=round((otooley_count/count)*100,4)
-        li_percent=round((li_count/count)*100,4)
-        #Formatting results to be to 3 decimal places
-        khan="{:.3f}".format(khan_percent)
-        correy="{:.3f}".format(correy_percent)
-        otooley="{:.3f}".format(otooley_percent)
-        li="{:.3f}".format(li_percent)
-        #Calculating the winner
-        winner_list = [khan, correy, otooley, li]
-        winner = max(winner_list)
-        #Changing format of winner's name
-        if winner == khan:
-            winner ="Khan"
-        elif winner == correy:
-            winner = "Correy"
-        elif winner == otooley:
-            winner = "O'tooley"
-        elif winner == li:
-            winner = "li"
+        #Setting candidate name variable to correct value
+        candidate_name = row[2]
+        #Adding candidate names to a unique list 
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+            #Setting the count variable that will count individual's votes
+            candidates[candidate_name] =0
+        #Telling the votes to be counted each time the candidate name appears
+        candidates[candidate_name] = candidates[candidate_name] + 1 
 
-            
-
-        
-
-    #Print out the results
-    print("Election Results")
-    print("---------------------------------")
-    print(f"Total Votes: {count}")
-    print("---------------------------------")
-    print(f"Khan: {khan}% ({khan_count})")
-    print(f"Correy: {correy}% ({correy_count})")
-    print(f"Li: {li}% ({li_count})")
-    print(f"O'Tooley: {otooley}% ({otooley_count})")
-    print("---------------------------------")
-    print(f'Winner: {winner}')
-    print("---------------------------------")
-
+# Beginning the Output file in order to write the print statements to the text file. 
+# Loops are used and therefore must be within the output writting "with" in order to then write them to the text file without loosing the information.
 # Specify the file to write to
 output_path = os.path.join("analysis", "output.txt")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, 'w') as txtfile:
 
+    #Printing the title of my analysis and the total vote count
+    print("Election Results")
+    print("---------------------------------")
+    print(f"Total Votes: {count}")
+    print("---------------------------------")
+    #Writting the prints aboce to the text file
+    txtfile.write(f'Election Results\r\n---------------------------------\r\nTotal Votes: {count}\r\n---------------------------------\r\n')
 
-    # Write the first row (column headers)
-    txtfile.write(f"Election Results\r\n---------------------------------\r\nTotal Votes: {count}\r\n---------------------------------\r\nKhan: {khan}% ({khan_count})\r\nCorrey: {correy}% ({correy_count})\r\nLi: {li}% ({li_count})\r\nO'Tooley: {otooley}% ({otooley_count})\r\n---------------------------------\r\nWinner: {winner}\r\n---------------------------------")
+    #Setting for loop to run through each name
+    for candidate in candidates:
+        #Collecting the number of votes per candidate
+        votes = candidates.get(candidate)
+        #Calculating the percentage of their votes 
+        vote_percentage = float(votes) / float(count) * 100
+        #Editing the format of the percentage to have 3 decimal places
+        vote_percentage_format="{:.3f}".format(vote_percentage)
+        #Printing the results of the analysis
+        print(f'{candidate}: {vote_percentage_format}% ({votes})')
+        #Writting the prints above to the textfile
+        txtfile.write(f'{candidate}: {vote_percentage_format}% ({votes})\r\n')
+    #Obtaining the winner of the election
+    winner = max(candidates,key=candidates.get)
+    #Printing the winner results
+    print("---------------------------------")
+    print(f'Winner: {winner}')
+    print("---------------------------------")
+    #Writting the prints above to the textfile
+    txtfile.write(f'---------------------------------\r\nWinner: {winner}\r\n---------------------------------')
+
